eb4b711295fe6adf3ab7b300bb9fb636a2a65355
EdisonDavidCode <edisondavidr.rd@gmail.com>
2024-02-17 21:51:04 -0400

    MovieMappers - MovieDB hacia Movie Entity

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -2,4 +2,5 @@ import 'package:cinemapedia_flutter/domain/entities/movies.dart';
 
 abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
+  
 }
---
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia_flutter/domain/entities/movies.dart';
+import 'package:cinemapedia_flutter/infraestructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBtoEntity(MovieMovieDB movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+          : 'https://cdn-icons-png.flaticon.com/512/2748/2748558.png',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: movieDB.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+          : 'https://cdn-icons-png.flaticon.com/512/2748/2748558.png',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+}
